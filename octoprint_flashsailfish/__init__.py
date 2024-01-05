433f9c5b428dfcf26a934f1cc4c141cf9e603ec2
wesk1 <wesk18@yahoo.com>
2024-01-05 10:50:30 -0800

    changed code

--- octoprint_flashsailfish/__init__.py
+++ octoprint_flashsailfish/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import
 
 import octoprint.plugin
 from octoprint.server.util.flask import restricted_access
@@ -12,121 +12,119 @@ from flask import request, make_response
 import requests
 import xmltodict
 
+
 class FlashSailfishPlugin(octoprint.plugin.SettingsPlugin,
-                          octoprint.plugin.AssetPlugin,
-                          octoprint.plugin.TemplatePlugin,
-                          octoprint.plugin.BlueprintPlugin):
+						  octoprint.plugin.AssetPlugin,
+						  octoprint.plugin.TemplatePlugin,
+						  octoprint.plugin.BlueprintPlugin):
 
-    def __init__(self, *args, **kwargs):
-        self.xml = None
-        self.firmware_info = None
+	def __init__(self, *args, **kwargs):
+		self.xml = None
+		self.firmware_info = None
 
-    ##~~ SettingsPlugin mixin
-    def get_settings_defaults(self, *args, **kwargs):
-        return dict(
-            url="http://s3.amazonaws.com/sailfish-firmware.polar3d.com/release/firmware.xml"
-        )
+	# ~~ SettingsPlugin mixin
+	def get_settings_defaults(self, *args, **kwargs):
+		return dict(
+			url="http://s3.amazonaws.com/sailfish-firmware.polar3d.com/release/firmware.xml"
+		)
 
-    ##~~ AssetPlugin mixin
-    def get_assets(self, *args, **kwargs):
-        return dict(
-            js=["js/flashsailfish.js"],
-            css=["css/flashsailfish.css"],
-            less=["less/flashsailfish.less"]
-        )
+	# ~~ AssetPlugin mixin
+	def get_assets(self, *args, **kwargs):
+		return dict(
+			js=["js/flashsailfish.js"],
+			css=["css/flashsailfish.css"],
+			less=["less/flashsailfish.less"]
+		)
 
-    ##~~ Softwareupdate hook
-    def get_update_information(self, *args, **kwargs):
-        return dict(
-            flashsailfish=dict(
-                displayName="Flash Sailfish",
-                displayVersion=self._plugin_version,
+	# ~~ Softwareupdate hook
+	def get_update_information(self, *args, **kwargs):
+		return dict(
+			flashsailfish=dict(
+				displayName="Flash Sailfish",
+				displayVersion=self._plugin_version,
 
-                # version check: github repository
-                type="github_release",
-                user="markwal",
-                repo="octoflashsailfish",
-                current=self._plugin_version,
+				# version check: github repository
+				type="github_release",
+				user="markwal",
+				repo="octoflashsailfish",
+				current=self._plugin_version,
 
-                # update method: pip
-                pip="https://github.com/wesk1/octoflashsailfish/archive/{target_version}.zip"
-            )
-        )
+				# update method: pip
+				pip="https://github.com/markwal/octoflashsailfish/archive/{target_version}.zip"
+			)
+		)
 
-    ##~~ Blueprint mixin
-    @BlueprintPlugin.route("/firmware_file", methods=["POST"])
-    @restricted_access
-    @admin_permission.require(403)
-    def firmware_file(self, *args, **kwargs):
-        pass
+	# ~~ Blueprint mixin
+	@BlueprintPlugin.route("/firmware_file", methods=["POST"])
+	@restricted_access
+	@admin_permission.require(403)
+	def firmware_file(self, *args, **kwargs):
+		pass
 
-    @BlueprintPlugin.route("/firmware_info", methods=["GET"])
-    @restricted_access
-    @admin_permission.require(403)
-    def get_firmware_info(self, *args, **kwargs):
-        return self._firmware_info()
+	@BlueprintPlugin.route("/firmware_info", methods=["GET"])
+	@restricted_access
+	@admin_permission.require(403)
+	def get_firmware_info(self, *args, **kwargs):
+		return self._firmware_info()
 
-    @BlueprintPlugin.route("/refresh_firmware_info", methods=["POST"])
-    @restricted_access
-    @admin_permission.require(403)
-    def refresh_firmware_info(self, *args, **kwargs):
-        if not request.json is None and "url" in request.json:
-            self._settings.set(["url"], request.json["url"])
-        self.xml = None
-        return self._firmware_info()
+	@BlueprintPlugin.route("/refresh_firmware_info", methods=["POST"])
+	@restricted_access
+	@admin_permission.require(403)
+	def refresh_firmware_info(self, *args, **kwargs):
+		if not request.json is None and "url" in request.json:
+			self._settings.set(["url"], request.json["url"])
+		self.xml = None
+		return self._firmware_info()
 
-    def _firmware_info(self):
-        if self.xml is None:
-            url = self._settings.get(["url"])
-            try:
-                self.xml = requests.get(url)
-            except:
-                self._logger.exception("Unable to retrieve firmware information from {0}".format(url))
-                return make_response("Unable to retrieve firmware information from {0}".format(url), 400)
-            try:
-                self.firmware_info = xmltodict.parse(self.xml.text)
-            except:
-                self._logger.exception("Retrieved firmware information from {0}, but was unable to understand the response.".format(url))
-                return make_response("Retrieved firmware information from {0}, but was unable to understand the response.".format(url), 400)
-        boards = dict()
-        for idx, board_xml in enumerate(self.firmware_info["boards"]["board"]):
-            if "@name" in board_xml and "firmware" in board_xml:
-                board = dict()
-                board["name"] = board_xml["@name"]
-                board["idx"] = idx
-                board["firmwares"] = dict()
-                if not isinstance(board_xml["firmware"], list):
-                    board_xml["firmware"] = [board_xml["firmware"]]
-                for firmware_xml in board_xml["firmware"]:
-                    if "@relpath" in firmware_xml and "@description" in firmware_xml and "@name" in firmware_xml:
-                        firmware = dict()
-                        firmware["relpath"] = firmware_xml["@relpath"]
-                        firmware["description"] = firmware_xml["@description"]
-                        board["firmwares"][firmware_xml["@name"]] = firmware
-                    else:
-                        self._logger.info("Skipping firmware %s", repr(firmware_xml))
-                boards[board_xml["@name"]] = board
-            else:
-                self._logger.info("Skipping board %s", repr(board_xml))
-        return flask.jsonify(boards)
-        
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+	def _firmware_info(self):
+		if self.xml is None:
+			url = self._settings.get(["url"])
+			try:
+				self.xml = requests.get(url)
+			except:
+				self._logger.exception("Unable to retrieve firmware information from {0}".format(url))
+				return make_response("Unable to retrieve firmware information from {0}".format(url), 400)
+			try:
+				self.firmware_info = xmltodict.parse(self.xml.text)
+			except:
+				self._logger.exception(
+					"Retrieved firmware information from {0}, but was unable to understand the response.".format(url))
+				return make_response(
+					"Retrieved firmware information from {0}, but was unable to understand the response.".format(url),
+					400)
+		boards = dict()
+		for idx, board_xml in enumerate(self.firmware_info["boards"]["board"]):
+			if "@name" in board_xml and "firmware" in board_xml:
+				board = dict()
+				board["name"] = board_xml["@name"]
+				board["idx"] = idx
+				board["firmwares"] = dict()
+				if not isinstance(board_xml["firmware"], list):
+					board_xml["firmware"] = [board_xml["firmware"]]
+				for firmware_xml in board_xml["firmware"]:
+					if "@relpath" in firmware_xml and "@description" in firmware_xml and "@name" in firmware_xml:
+						firmware = dict()
+						firmware["relpath"] = firmware_xml["@relpath"]
+						firmware["description"] = firmware_xml["@description"]
+						board["firmwares"][firmware_xml["@name"]] = firmware
+					else:
+						self._logger.info("Skipping firmware %s", repr(firmware_xml))
+				boards[board_xml["@name"]] = board
+			else:
+				self._logger.info("Skipping board %s", repr(board_xml))
+		return flask.jsonify(boards)
+
+
 __plugin_name__ = "Flash Sailfish"
 
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncom__ = ">=2.7,<4"  # python 2 and 3
+
 
 def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = FlashSailfishPlugin()
+	global __plugin_implementation__
+	__plugin_implementation__ = FlashSailfishPlugin()
 
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 ########################################################################################################################
 
@@ -7,18 +6,19 @@ plugin_identifier = "flashsailfish"
 plugin_package = "octoprint_flashsailfish"
 plugin_name = "OctoPrint-FlashSailfish"
 plugin_version = "0.1.1"
-plugin_description = """This plugin checks for updates of the Sailfish firmware and will offer to flash the new firmware onto your printer when a new version is available."""
+plugin_description = """This plugin checks for updates of the Sailfish firmware and will offer to flash the new
+firmware onto your printer when a new version is available."""
 plugin_author = "Mark Walker"
 plugin_author_email = "markwal@hotmail.com"
 plugin_url = "https://github.com/wesk1/octoflashsailfish"
-plugin_license = "AGPLv3"
+plugin_license = "GPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = []
 
-### --------------------------------------------------------------------------------------------------------------------
-### More advanced options that you usually shouldn't have to touch follow after this point
-### --------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
+# More advanced options that you usually shouldn't have to touch follow after this point
+# --------------------------------------------------------------------------------------------------------------------
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
@@ -35,22 +35,27 @@ plugin_ignored_packages = []
 # default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
 # octoprint.util.dict_merge.
 #
-# Example:
-#     plugin_requires = ["someDependency==dev"]
-#     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
+# Example: plugin_requires = ["someDependency==dev"] additional_setup_parameters = {"dependency_links": [
+# "https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
 additional_setup_parameters = {}
 
 ########################################################################################################################
 
+# Standard library imports
+import sys
+
+# Related third-party imports
 from setuptools import setup
 
 try:
     import octoprint_setuptools
-except:
+except ImportError:
     print("Could not import OctoPrint's setuptools, are you sure you are running that under "
           "the same python installation that OctoPrint is installed under?")
-    import sys
     sys.exit(-1)
+
+# Local application/library specific imports
+from octoprint.util import dict_merge
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
     identifier=plugin_identifier,
@@ -69,7 +74,6 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
 )
 
 if len(additional_setup_parameters):
-    from octoprint.util import dict_merge
     setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
