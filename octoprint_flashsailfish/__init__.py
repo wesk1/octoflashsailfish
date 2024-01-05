84fdafd06806b9e74a49156b73e44295f245c274
wesk1 <wesk18@yahoo.com>
2024-01-05 02:11:50 -0800

    update code

--- octoprint_flashsailfish/__init__.py
+++ octoprint_flashsailfish/__init__.py
@@ -109,8 +109,18 @@ class FlashSailfishPlugin(octoprint.plugin.SettingsPlugin,
             else:
                 self._logger.info("Skipping board %s", repr(board_xml))
         return flask.jsonify(boards)
-
+        
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Flash Sailfish"
+
+# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
+# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
+# compatibility flags according to what Python versions your plugin supports!
+#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+#__plugin_pythoncompat__ = ">=3,<4" # only python 3
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
     global __plugin_implementation__
