7fb3132f0c8dd19f884e72da8bf52b1b3003d0c8
wesk1 <wesk18@yahoo.com>
2024-01-05 18:43:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
 	url=plugin_url,
 	license=plugin_license,
 	requires=plugin_requires,
-	additional_packages=plugin_addtional_packages,
+	additional_packages=plugin_additional_packages,
 	ignored_packages=plugin_ignored_packages,
 	additional_data=plugin_additional_data
 )
