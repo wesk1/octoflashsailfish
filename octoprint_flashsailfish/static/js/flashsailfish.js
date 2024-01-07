626e225c6c58c3d779f191eef63c4aa86dc68276
wesk1 <wesk18@yahoo.com>
2024-01-06 23:03:25 -0800

    Update flashsailfish.js

--- octoprint_flashsailfish/static/js/flashsailfish.js
+++ octoprint_flashsailfish/static/js/flashsailfish.js
@@ -6,7 +6,7 @@
  */
 $(function() {
     function FlashsailfishViewModel(parameters) {
-        var self = this;
+        const self = this;
 
         self.settings = parameters[0];
 
@@ -16,22 +16,26 @@ $(function() {
         self.version = ko.observable(undefined);
         self.firmware_path = ko.observable(undefined);
 
-        self.firmware_info = undefined;
+        self.firmware_info = /** @type {any} */ (undefined);
 
-        self.custom_selected = ko.computed(function() {
-            return self.version() == "custom";
-        }, self);
+        self.custom_selected = ko.computed(() => {
+            return self.version() === "custom";
+        });
 
         self.flash_firmware = function() {
+            // Your implementation here
         };
 
         self.refresh_firmware_xml = function() {
+            // Your implementation here
         };
 
         self.refresh_observables = function() {
-            if (self.firmware_info != undefined) {
-                for (board in self.firmware_info) {
-                    self.boards.push(board);
+            if (self.firmware_info !== undefined) {
+                for (const board in self.firmware_info) {
+                    if (self.firmware_info.hasOwnProperty(board)) {
+                        self.boards.push(board);
+                    }
                 }
                 self.boards.sort();
             }
@@ -39,7 +43,7 @@ $(function() {
 
         self.fetch_firmware_info = function() {
             $.getJSON("/plugin/flashsailfish/firmware_info", function(data) {
-                self.firmware_info = data;
+                self.firmware_info = /** @type {any} */ (data);
                 self.refresh_observables();
             });
         };
@@ -47,10 +51,10 @@ $(function() {
         self.onSettingsShown = self.fetch_firmware_info;
     }
 
-    // view model class, parameters for constructor, container to bind to
+    // View model class, parameters for constructor, container to bind to
     OCTOPRINT_VIEWMODELS.push([
         FlashsailfishViewModel,
-        [ "settingsViewModel" ],
-        [ "#settings_plugin_flashsailfish" ]
+        ["settingsViewModel"],
+        ["#settings_plugin_flashsailfish"]
     ]);
 });
