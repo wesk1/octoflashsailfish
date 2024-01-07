6b35995008d481895ca9611fd3453a13ce7dc60e
wesk1 <wesk18@yahoo.com>
2024-01-07 11:32:30 -0800

    Update flashsailfish.js

--- octoprint_flashsailfish/static/js/flashsailfish.js
+++ octoprint_flashsailfish/static/js/flashsailfish.js
@@ -26,34 +26,13 @@ $(function() {
         };
 
         self.refresh_firmware_xml = function() {
-    $.getJSON("/plugin/flashsailfish/firmware_info", function(data) {
-        self.firmware_info = data;
-        self.refresh_observables();
-    });
-};
-        self.update_firmware = function() {
-            $.ajax({
-                url: API_BASEURL + "plugin/flashsailfish/update_firmware",
-                type: "POST",
-                success: function(data) {
-                    // Handle success
-                    console.log("Firmware update status: " + data.status);
-                    // You can update the UI based on the firmware update status if needed
-                },
-                error: function(xhr, status, error) {
-                    // Handle error
-                    console.error("Firmware update failed: " + error);
-                }
-            });
         };
 
         self.refresh_observables = function() {
     self.boards.removeAll();  // Clear the array first
-
-    if (self.firmware_info !== undefined) {
+            if (self.firmware_info !== undefined) {
         console.log("Firmware Info:", self.firmware_info);  // Add this line for debugging
-
-        for (const board in self.firmware_info) {
+                for (const board in self.firmware_info) {
             if (self.firmware_info.hasOwnProperty(board)) {
                 console.log("Adding board:", board);  // Add this line for debugging
                 self.boards.push(board);
@@ -64,7 +43,7 @@ $(function() {
 
         self.boards.sort();
     }
-};
+        };
 
         self.fetch_firmware_info = function() {
             $.getJSON("/plugin/flashsailfish/firmware_info", function(data) {
