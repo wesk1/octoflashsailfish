c9521905a96b17ccfde7ee3415968f84efa32da9
wesk1 <wesk18@yahoo.com>
2024-01-07 03:28:41 -0800

    Update flashsailfish.js

--- octoprint_flashsailfish/static/js/flashsailfish.js
+++ octoprint_flashsailfish/static/js/flashsailfish.js
@@ -1,21 +1,13 @@
-/*
- * View model for OctoPrint-FlashSailfish
- *
- * Author: Mark Walker
- * License: GPLv3
- */
 $(function() {
     function FlashsailfishViewModel(parameters) {
         const self = this;
 
         self.settings = parameters[0];
-
         self.boards = ko.observableArray([]);
         self.board = ko.observable(undefined);
         self.versions = ko.observableArray([]);
         self.version = ko.observable(undefined);
         self.firmware_path = ko.observable(undefined);
-
         self.firmware_info = undefined;
 
         self.custom_selected = ko.computed(() => {
@@ -23,34 +15,26 @@ $(function() {
         });
 
         self.flash_firmware = function() {
+            // Placeholder for firmware flashing logic
         };
 
-        self.refresh_firmware_xml = function() {
-    $.getJSON("/plugin/flashsailfish/firmware_info", function(data) {
-        self.firmware_info = data;
-        self.refresh_observables();
-    });
-};
+        self.refresh_observables = function() {
+            self.boards.removeAll();
 
-self.refresh_observables = function() {
-    self.boards.removeAll();  // Clear the array first
+            if (self.firmware_info !== undefined) {
+                console.log("Firmware Info:", self.firmware_info);
 
-    if (self.firmware_info !== undefined) {
-        console.log("Firmware Info:", self.firmware_info);  // Add this line for debugging
+                for (const board in self.firmware_info) {
+                    if (self.firmware_info.hasOwnProperty(board)) {
+                        console.log("Adding board:", board);
+                        self.boards.push(board);
+                    }
+                }
 
-        for (const board in self.firmware_info) {
-            if (self.firmware_info.hasOwnProperty(board)) {
-                console.log("Adding board:", board);  // Add this line for debugging
-                self.boards.push(board);
+                console.log("Sorted Boards:", self.boards());
+                self.boards.sort();
             }
-        }
-
-        console.log("Firmware Info:", self.firmware_info);
-        console.log("Sorted Boards:", self.boards());
-
-        self.boards.sort();
-    }
-};
+        };
 
         self.fetch_firmware_info = function() {
             $.getJSON("/plugin/flashsailfish/firmware_info", function(data) {
@@ -62,7 +46,6 @@ self.refresh_observables = function() {
         self.onSettingsShown = self.fetch_firmware_info;
     }
 
-    // view model class, parameters for constructor, container to bind to
     OCTOPRINT_VIEWMODELS.push([
         FlashsailfishViewModel,
         [ "settingsViewModel" ],
