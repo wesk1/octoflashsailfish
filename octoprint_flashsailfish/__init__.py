61575a6ee6ec3a25f4ca3b8ac1f68c3d841df493
wesk1 <wesk18@yahoo.com>
2024-01-14 00:23:10 -0800

    Update __init__.py

--- octoprint_flashsailfish/__init__.py
+++ octoprint_flashsailfish/__init__.py
@@ -112,10 +112,10 @@ class FlashSailfishPlugin(octoprint.plugin.BlueprintPlugin,
             firmware_content = response.content
             firmware_info = xmltodict.parse(response.txt)
             relpath = firmware_info["firmware"]["@relpath"]
-            filename = secure_filename(relpath)
+            """filename = secure_filename(relpath)"""
 
             # Save the firmware to the specified destination directory
-            with open(os.path.join(destination_dir, filename), "wb") as firmware_file:
+            with open(os.path.join(destination_dir, relpath), "wb") as firmware_file:
                 firmware_file.write(firmware_content)
 
             return flask.jsonify({"message": "Firmware download initiated"})
